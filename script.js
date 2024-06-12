14b2755f6c9722bdc92a626b8036ba3ed0d578b3
noordinq <nooshqureish@gmail.com>
2024-06-12 16:10:44 +0300

    js functionality added

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x");
     navBar.classList.toggle("active");
 }
+
+// Dark mode toggle
+let darkModeIcon = document.querySelector("#darkMode-icon");
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bxs-sun');
+    document.body.classList.toggle('dark-mode');
+}
